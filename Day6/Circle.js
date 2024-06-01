4b57e45ecea7ae16b8f1b6f38b25a40151c839a1
DURUVANTHRAJ <114046619+DURUVANTHRAJBG@users.noreply.github.com>
2024-06-01 15:52:31 +0530

    Update Circle.js

---
+++ Day6/Circle.js
@@ -0,0 +1,13 @@
+Circle
+-radius: double = 1.0
+-color: String = “red”
++Circle()
++Circle (radius:double)
++Circle (radius: double, color:String)
++getRadius(): double
++setRadius (radius: double):void
++getColor ():String
++setColor (color: String): void
++toString():String “Circle[radius =?, color =?]”
++getArea (): double
++getCircumference(): double
